be44a9efc6480349f6ca1b79512a4a0d600e9be7
Andreas Neumann <andreas@ksg-ilmenau.de>
2013-06-24 02:32:18 +0200

    einen schritt weiter, vorbereitungen für die sqliteanbindung

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,10 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var db = null;
+var dataversion = 1;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,6 +37,15 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+		// Datenbankverbindung
+		db = window.openDataBase("Hochschulkatholik","1.0","App Datenbank",1000000);
+		
+		// Tabellen erstellen
+		db.transaction(initDB, errorCB, successCB);
+		
+		// Prüfe Datenversion
+		db.transaction(checkVersion, errorCB, successCB);
+		
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -47,3 +60,33 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+function initDB(tx) {
+	tx.executeSql('CREATE TABLE IF NOT EXISTS dataInfo (id TEXT PRIMARY KEY, data TEXT)');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS gemeinde (id TEXT PRIMARY KEY, kurz TEXT NOT NULL, lang TEXT NOT NULL, strasse TEXT NOT NULL, ort TEXT NOT NULL, plz TEXT NOT NULL, patron TEXT, url TEXT, lat REAL, lon REAL)');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS akhVeranstaltung (id TEXT PRIMARY KEY, start INTEGER NOT NULL, ende TEXT NOT NULL, titel TEXT NOT NULL, adresse TEXT NOT NULL, beschreibung TEXT, foto TEXT, lat REAL, lon REAL)');
+}
+
+function checkVersion(tx) {
+	tx.executeSql("SELECT id FROM dataInfo WHERE id = 'version' AND data < "+dataVersion, [], checkSuccess, checkError);
+}
+
+function successCB() {}
+
+
+function errorCB(err) {
+}
+
+function checkSuccess(tx, results) {
+	if(results.rows.length > 0) {
+		initDatabase();
+	}
+}
+
+function checkError(err) {
+	initDatabase();
+}
+
+function initDatabase() {
+	
+}
