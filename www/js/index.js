d5394e2d4b2bef5c983d5bdae15d24cb1265c872
Andreas Neumann <andreas@ksg-ilmenau.de>
2013-06-26 22:18:24 +0200

    neue version

--- www/js/index.js
+++ www/js/index.js
@@ -41,12 +41,46 @@ var app = {
 		db = window.openDataBase("Hochschulkatholik","1.0","App Datenbank",1000000);
 		
 		// Tabellen erstellen
-		db.transaction(initDB, errorCB, successCB);
+		db.transaction(function(tx) {
+			tx.executeSql('CREATE TABLE IF NOT EXISTS dataInfo (id TEXT PRIMARY KEY, data TEXT)');
+			tx.executeSql('CREATE TABLE IF NOT EXISTS gemeinde (id TEXT PRIMARY KEY, kurz TEXT NOT NULL, lang TEXT NOT NULL, strasse TEXT NOT NULL, ort TEXT NOT NULL, plz TEXT NOT NULL, patron TEXT, url TEXT, lat REAL, lon REAL)');
+			tx.executeSql('CREATE TABLE IF NOT EXISTS akhVeranstaltung (id TEXT PRIMARY KEY, start INTEGER NOT NULL, ende TEXT NOT NULL, titel TEXT NOT NULL, adresse TEXT NOT NULL, beschreibung TEXT, foto TEXT, lat REAL, lon REAL)');
+		});
 		
 		// Prüfe Datenversion
-		db.transaction(checkVersion, errorCB, successCB);
+		db.transaction(function(tx) {
+			tx.executeSql("SELECT id FROM dataInfo WHERE id = 'version' AND data < "+dataVersion, [], function(tx,rs) {
+				if(rs.rows.length > 0) {
+					$.get(url:'data.json',success:function(data){
+						var json = $.parseJSON(data);
+						db.transaction(function(tx) {
+							tx.executeSql('INSERT OR REPLACE INTO dataInfo (id, data) values (\'version\', \''+json.dataInfo.version+'\')');
+							tx.executeSql('DROP TABLE gemeinde');
+							tx.executeSql('CREATE TABLE gemeinde (id TEXT PRIMARY KEY, kurz TEXT NOT NULL, lang TEXT NOT NULL, strasse TEXT NOT NULL, ort TEXT NOT NULL, plz TEXT NOT NULL, patron TEXT, url TEXT, lat REAL, lon REAL)');
+						});
+						for(var i = 0; i < json.gemeinde.length; i++) {
+							var gemeinde = json.gemeinde[i];
+							var id = (typeof gemeinde.id != 'undefined') ? gemeinde.id : "";
+							var kurz = (typeof gemeinde.kurz != 'undefined') ? gemeinde.kurz : "";
+							var lang = (typeof gemeinde.lang != 'undefined') ? gemeinde.lang : "";
+							var strasse = (typeof gemeinde.strasse != 'undefined') ? gemeinde.strasse : "";
+							var ort = (typeof gemeinde.ort != 'undefined') ? gemeinde.ort : "";
+							var plz = (typeof gemeinde.plz != 'undefined') ? gemeinde.plz : "";
+							var patron = (typeof gemeinde.patron != 'undefined') ? gemeinde.patron : "";
+							var url = (typeof gemeinde.url != 'undefined') ? gemeinde.url : "";
+							var lat = (typeof gemeinde.lat != 'undefined') ? gemeinde.lat : "";
+							var lon = (typeof gemeinde.lon != 'undefined') ? gemeinde.lon : "";
+							db.transaction(function(tx) {
+								tx.executeSql('INSERT OR REPLACE INTO gemeinde (id, kurz, lang, strasse, ort, plz, patron, url, lat, lon) values (\''+id+'\', \''+kurz+'\', \''+lang+'\', \''+strasse+'\', \''+ort+'\', \''+plz+'\', \''+patron+'\', \''+url+'\', \''+lat+'\', \''+lon+'\')');
+							});
+						}
+					});
+				}
+			});
+		});
 		
         app.receivedEvent('deviceready');
+		
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -62,9 +96,7 @@ var app = {
 };
 
 function initDB(tx) {
-	tx.executeSql('CREATE TABLE IF NOT EXISTS dataInfo (id TEXT PRIMARY KEY, data TEXT)');
-	tx.executeSql('CREATE TABLE IF NOT EXISTS gemeinde (id TEXT PRIMARY KEY, kurz TEXT NOT NULL, lang TEXT NOT NULL, strasse TEXT NOT NULL, ort TEXT NOT NULL, plz TEXT NOT NULL, patron TEXT, url TEXT, lat REAL, lon REAL)');
-	tx.executeSql('CREATE TABLE IF NOT EXISTS akhVeranstaltung (id TEXT PRIMARY KEY, start INTEGER NOT NULL, ende TEXT NOT NULL, titel TEXT NOT NULL, adresse TEXT NOT NULL, beschreibung TEXT, foto TEXT, lat REAL, lon REAL)');
+	
 }
 
 function checkVersion(tx) {
@@ -88,5 +120,9 @@ function checkError(err) {
 }
 
 function initDatabase() {
-	
+	$.get(url:'data.json',success:function(data){
+		var json = $.parseJSON(data);
+		
+		$('#shortlink').attr("href",json.short);
+	});
 }
