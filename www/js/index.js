2708ece75ce12527152f957f956cf19aeb57e5f8
Andreas Neumann <andreas@ksg-ilmenau.de>
2013-06-27 20:32:06 +0200

    next step

--- www/js/index.js
+++ www/js/index.js
@@ -19,6 +19,8 @@
 
 var db = {};
 var dataversion = '1';
+var lat = 50;
+var lon = 10;
 
 var app = {
     // Application Constructor
@@ -42,7 +44,16 @@ var app = {
     }
 };
 
+function geolocationSuccess(position) {
+	lat = position.coords.latitude;
+	lon = position.coords.longitude;
+}
+
 function onDeviceReady() {
+	// Geo-Daten
+	console.log('Position ermitteln');
+	navigator.geolocation.getCurrentPosition(geolocationSuccess);
+	
 	// Datenbankverbindung
 	console.log('Datenbankverbindung');
 	db = window.openDatabase("kath", "1.0", "Sync Demo DB", 200000);
@@ -101,15 +112,48 @@ function onDeviceReady() {
 }
 
 function gemeindefill() {
+	var kurzestrecke = 10000;
+	var nextksg = '';
 	$('#gemeindeliste').html('');
 	console.log('gemeindeliste füllen');
 	db.transaction(function(tx) {
-		tx.executeSql("SELECT id, kurz, ort FROM gemeinde ORDER BY ort", [], function(tx,rs) {
+		tx.executeSql("SELECT id, kurz, ort, lat, lon FROM gemeinde ORDER BY ort", [], function(tx,rs) {
 			for (var i = 0; i < rs.rows.length; i++) {
 				var gemeinde = rs.rows.item(i);
-				$('#gemeindeliste').append('<li><a href="#gemeinde" onclick="setGemeinde(\''+gemeinde.id+'\')">'+gemeinde.kurz+' '+gemeinde.ort+'</a></li>');
+				console.log('gemeindeliste einfüllen:'+gemeinde.id);
+				var strecke = entfernungBerechnen(gemeinde.lat,gemeinde.lon);
+				$('#gemeindeliste').append('<li><a href="#gemeinde" onclick="setGemeinde(\''+gemeinde.id+'\')">'+gemeinde.kurz+' '+gemeinde.ort+' <span class="ui-li-count">'+strecke+'km</span></a></li>');
+				if(strecke < kurzestrecke) { 
+					nextksg = gemeinde.id;
+					kurzestrecke = strecke;
+				}
+				console.log(gemeinde.id+': '+strecke+' km');
 			}
+			console.log('gemeindedaten füllen');
+			tx.executeSql("SELECT * FROM gemeinde WHERE id = '"+nextksg+"'", [], function(tx,rs) {
+				for (var i = 0; i < rs.rows.length; i++) {
+					var gemeinde = rs.rows.item(i);
+					$('#maingemeindename').html(gemeinde.lang+' &bdquo;'+gemeinde.patron+'&ldquo;');
+					$('#maingemeindeadresse').html('<strong>'+gemeinde.kurz+' '+gemeinde.ort+'</strong><br/>'+gemeinde.strasse+'<br/>'+gemeinde.plz+' '+gemeinde.ort+'<br/><br/><a href=\''+gemeinde.url+'\'>'+gemeinde.url+'</a>');
+					$('#maingemeindebleiste_karte').attr('href','geo:'+gemeinde.lat+','+gemeinde.lon);
+					if(gemeinde.configurl.length > 0 ) { $('#maingemeindebleiste_zusatz').attr('onclick',''); }
+					else {$('#maingemeindebleiste_zusatz').addClass('hidden');}
+					$('#maingemeindebleiste_fav').attr('onclick','makeFav("'+gemeinde.id+'")');
+					//$('#gemeindebleiste').html('<a href="geo:'+gemeinde.lat+','+gemeinde.lon+'" data-icon="map" data-role="button">Karte</a>');
+				}
+			});
+			tx.executeSql("SELECT id FROM fav WHERE id = '"+nextksg+"'" , [], function(tx,rs) {
+				console.log('Soviele gibt es: '+rs.rows.length);
+				if(rs.rows.length == 0) {
+					$('#maingemeindebleiste_fav').removeClass('ui-btn-active');
+				}
+				else {
+					$('#maingemeindebleiste_fav').addClass('ui-btn-active');
+				}
+			});
 		});
+		// Favoriten aufbauen
+		makeFavList();
 	});
 }
 
@@ -148,12 +192,14 @@ function makeFav(id) {
 				db.transaction(function(tx) {
 					console.log('Erstelle Fav');
 					tx.executeSql('INSERT INTO fav (id) values (\''+id+'\');');
+					makeFavList();
 				});
 			}
 			else {
 				db.transaction(function(tx) {
 					console.log('Lösche Fav');
 					tx.executeSql('DELETE FROM fav WHERE id = \''+id+'\';');
+					makeFavList();
 				}, function (tx, err) { 
 					console.log("Rückgabe: "+tx.code+' '+tx.message); 
 				});
@@ -161,6 +207,56 @@ function makeFav(id) {
 		});
 	});
 }
+
+function makeFavList() {
+	$('#fav_content').html('');
+	db.transaction(function(tx) {
+		tx.executeSql("SELECT id FROM fav ORDER BY id" , [], function(tx,rs) {
+			console.log('Soviele gibt es: '+rs.rows.length);
+			for(var i = 0; i < rs.rows.length; i++) {
+				var favgemeinde = rs.rows.item(i);
+				console.log('fav: '+favgemeinde.id);
+					console.log('fav: '+favgemeinde.id);
+					tx.executeSql("SELECT * FROM gemeinde WHERE id = '"+favgemeinde.id+"'", [], function(tx2,rs2) {
+						for (var j = 0; j < rs2.rows.length; j++) {
+							var g = rs2.rows.item(j);
+							console.log('fav: '+g.id);
+							var title = $('<h2/>').html(g.kurz+' '+g.ort);
+							var name = $('<h3/>').html(g.lang+' &bdquo;'+g.patron+'&ldquo;');
+							var adresse = $('<p/>').html('<strong>'+g.kurz+' '+g.ort+'</strong><br/>'+g.strasse+'<br/>'+g.plz+' '+g.ort+'<br/><br/><a href=\''+g.url+'\'>'+g.url+'</a>');
+							var geo = $('<a/>').attr('href','geo:'+g.lat+','+g.lon).attr('class','CustomIconButton').attr('data-role',"button").html('p');
+							var zdata = $('<a/>').attr('href','#').attr('class','CustomIconButton').attr('data-role',"button").html('i');
+							if(g.configurl.length > 0 ) { zdata.attr('onclick',''); }
+							else {zdata.addClass('hidden');}
+							var fav = $('<a/>').attr('href','#').attr('class','CustomIconButton').attr('data-role',"button").addClass('ui-btn-active').html('s').click(function () {makeFav(g.id)});
+							var bleiste = $('<div/>').attr('data-role',"controlgroup").attr('data-type','horizontal').append(geo).append(zdata).append(fav);
+							var p = $('<p/>').append(name).append(adresse).append(bleiste);
+							var collapsrow = $('<div/>').attr('data-role',"collapsible").append(title).append(p).addClass('favCollaps');
+							$('#fav_content').append(collapsrow);
+						}
+						console.log('refresh collaps');
+						$('#fav_content').trigger( "create" );  
+					});
+			}
+		});
+	});
+}
+
+function entfernungBerechnen(lat2,lon2) {
+	erdRadius = 6371; 
+	meineLongitude = lon * (Math.PI/180);
+	meineLatitude = lat * (Math.PI/180);
+	lon2 = lon2 * (Math.PI/180);
+	lat2 = lat2 * (Math.PI/180);
+	x0 = meineLongitude * erdRadius * Math.cos(meineLatitude);
+	y0 = meineLatitude * erdRadius;
+	x1 = lon2 * erdRadius * Math.cos(lat2);
+	y1 = lat2 * erdRadius;
+	dx = x0 - x1;
+	dy = y0 - y1;
+	return Math.round(Math.sqrt((dx*dx) + (dy*dy)));
+	return Math.round(d*10)/10;
+};
 /*
 $(document).ready(function() {
 onDeviceReady();
